d638e2b505666d5fd535565319bf30bbe66ee158
Marco Polo Viana <marcopoloviana@hotmail.com>
2017-09-28 12:58:44 -0300

    Implementação da interface IIcms na classe Icms51

--- FiscalNet/Implementacoes/Icms/Icms51.cs
+++ FiscalNet/Implementacoes/Icms/Icms51.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FiscalNET.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FiscalNet.Implementacoes.Icms
 {
-    public class Icms51
+    public class Icms51 : IIcms
     {
         private decimal AliqIcmsProprio { get; set; }
         private decimal AliqDifIcms { get; set; }
