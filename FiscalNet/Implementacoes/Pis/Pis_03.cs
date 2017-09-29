eba5e83afbbfa458d5778133a731a408bf3ade3d
Marco Polo Viana <marcopoloviana@hotmail.com>
2017-09-29 11:30:39 -0300

    Implementacao da classe Pis03

--- FiscalNet/Implementacoes/Pis/Pis_03.cs
+++ FiscalNet/Implementacoes/Pis/Pis_03.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FiscalNET.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace FiscalNet.Implementacoes.Pis
 {
-    class Pis_03
+    public class Pis_03 : IPis
     {
+        private decimal AliqEspecificaPis { get; set; }
+        private decimal ValorIpi { get; set; }
+        private decimal DespesasAcessorias { get; set; }
+        private decimal ValorFrete { get; set; }
+        private decimal ValorProduto { get; set; }
+        private decimal ValorSeguro { get; set; }
+        private decimal QuantidadeProduto { get; set; }
+
+        public Pis_03(decimal aliqEspecificaPis,
+            decimal despesasAcessorias,
+            decimal valorFrete, decimal valorProduto,
+            decimal valorSeguro, decimal quantidadeProduto)
+        {
+            this.AliqEspecificaPis  = aliqEspecificaPis;
+            this.DespesasAcessorias = despesasAcessorias;
+            this.ValorFrete         = valorFrete;
+            this.ValorProduto       = valorProduto;
+            this.ValorSeguro        = valorSeguro;
+            this.QuantidadeProduto  = quantidadeProduto;
+
+        }
+        public decimal BaseCalculo()
+        {
+            decimal BasePis = this.QuantidadeProduto;
+            return BasePis;
+        }
+
+        public decimal Valor()
+        {
+            return (this.BaseCalculo() * AliqEspecificaPis);
+        }
     }
 }
